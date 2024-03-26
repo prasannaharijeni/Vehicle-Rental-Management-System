75c855ae67ae32758c0b46d31a7859b3ab2bb889
prasanna <hariprasannajeni@gmail.com>
2024-03-26 19:51:55 -0400

    The general Vehicle class was created to get the basic details such as model name, manufacturer, model year, rental price and to display the same.

--- Vehicle Rental Management System/Program.cs
+++ Vehicle Rental Management System/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+public class Vehicle
+{
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    private decimal rentalPrice;
+
+    //To get the rental price of each vehicle
+    public decimal RentalPrice
+    {
+        get { return rentalPrice; }
+        set { rentalPrice = value; }
+    }
+
+    //To get the model, manufacturer, model year, and rental price of the renting vehicle
+    public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+        RentalPrice = rentalPrice;
+    }
+
+    //To display the details of the vehicle
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: {RentalPrice}");
+    }
+}
