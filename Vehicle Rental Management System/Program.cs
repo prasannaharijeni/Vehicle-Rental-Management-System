e8520bf52e372903f3cc15789dbbe9c5bf90592a
prasanna <hariprasannajeni@gmail.com>
2024-03-26 19:55:26 -0400

    creating the motorcycle class

--- Vehicle Rental Management System/Program.cs
+++ Vehicle Rental Management System/Program.cs
@@ -72,3 +72,24 @@ public class Truck : Vehicle
         Console.WriteLine($"\nCapacity: {Capacity} tons, Truck Type: {TruckType}, Four-Wheel Drive: {FourWheelDrive}");
     }
 }
+
+//Creating the Motorcycle class to inherit all the information from the vehicle class
+public class Motorcycle : Vehicle
+{
+    public int EngineCapacity { get; set; }
+    public string FuelType { get; set; }
+    public bool HasFairing { get; set; }
+
+    public Motorcycle(string model, string manufacturer, int year, decimal rentalPrice, int engineCapacity, string fuelType, bool hasFairing) : base(model, manufacturer, year, rentalPrice)
+    {
+        EngineCapacity = engineCapacity;
+        FuelType = fuelType;
+        HasFairing = hasFairing;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Engine Capacity: {EngineCapacity}cc, Fuel Type: {FuelType}, Has Fairing: {HasFairing}");
+    }
+}
